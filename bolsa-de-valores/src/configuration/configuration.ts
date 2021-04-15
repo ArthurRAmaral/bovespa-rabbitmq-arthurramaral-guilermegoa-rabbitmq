6d9a6d4209a072d3ed3d1e00e5903ba95c9cd472
Arthur Rocha Amaral <ruhtraamaral@htmail.com>
2021-04-15 19:34:38 -0300

    fix: configure rmq connection

--- bolsa-de-valores/src/configuration/configuration.ts
+++ bolsa-de-valores/src/configuration/configuration.ts
@@ -1,16 +1,16 @@
 export default () => ({
   rabbitmq: {
     exchanges: {
-      compra: process.env.RMQ_EXCHANGE_COMPRA || "compra",
-      venda: process.env.RMQ_EXCHANGE_VENDA || "venda",
+      compra: process.env.RMQ_EXCHANGE_COMPRA || 'compra',
+      venda: process.env.RMQ_EXCHANGE_VENDA || 'venda',
     },
     prefix: {
-      compra: process.env.RMQ_PREFIX_COMPRA || "compra",
-      venda: process.env.RMQ_PREFIX_VENDA || "venda",
+      compra: process.env.RMQ_PREFIX_COMPRA || 'compra',
+      venda: process.env.RMQ_PREFIX_VENDA || 'venda',
     },
-    user: process.env.RMQ_USER || "guest",
-    password: process.env.RMQ_PASSWORD || "guest",
-    url: process.env.RMQ_URL || "rabbitmq",
-    port: process.env.RMQ_PORT || "5672",
-  }
+    user: process.env.RMQ_USER || 'guest',
+    password: process.env.RMQ_PASSWORD || 'guest',
+    url: process.env.RMQ_HOST || 'rabbitmq',
+    port: process.env.RMQ_PORT || '5672',
+  },
 });
--- corretora/.docker/entrypoint.sh
+++ corretora/.docker/entrypoint.sh
@@ -2,6 +2,13 @@
 
 PATH=${PATH}:$HOME/app/node_modules/.bin
 
+RMQ_HOST=${RMQ_HOST:?}
+RMQ_PORT=${RMQ_PORT:?}
+
+until nc -z ${RMQ_HOST} ${RMQ_PORT}; do
+  sleep 0.1
+done
+
 yarn install --silent
 
 exec "$@"
